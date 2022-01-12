95c9a47ee8bcc4e9d2cc4a3ffe69469ae4822b88
seekwe <seekwe@gmail.com>
2022-01-12 14:11:30 +0800

    feat: 精简默认配置

--- public/index.php
+++ public/index.php
@@ -21,49 +21,17 @@ require __DIR__ . '/../vendor/autoload.php';
 // 缓存目录,请保证有写入权限
 defined('ZLS_STORAGE_PATH') || define('ZLS_STORAGE_PATH', ZLS_APP_PATH . '../storage/');
 Zls::initialize()
-    // 设置缓存目录
-    ->setStorageDirPath(ZLS_STORAGE_PATH)
     // 设置指令
     // ->setCommands([])
     // 设置中间件
     // ->setHttpMiddleware(['\Middleware\Http'])
-    // 设置错误级别,也就是error_reporting()的参数,只有此级别的错误才会触发下面的错误显示控制处理类
+    // 设置错误级别
     //->setExceptionLevel(E_ALL ^ E_DEPRECATED)
-    // 开启调试日志
-    ->setTraceStatus(true)
-    // 日志处理
-    //->setTraceStatusCallBack(function ($log,$type){})
-    // 设置自动Api文档访问toekn
-    // ->setApiDocToken('')
-    // 设置URL后缀
-    ->setMethodUriSubfix('.html')
-    // 维护模式
-    //->setIsMaintainMode(false)
-    // 维护模式IP白名单
-    //->setMaintainIpWhitelist('maintainIpWhitelist')
-    // 维护模式处理方法
-    ->setMaintainModeHandle(new \Zls_Maintain_Handle_Default())
-    //->setClientIpConditions(['HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP', 'REMOTE_ADDR'], ['HTTP_X_FORWARDED_FOR'])
-    // 注册路由器
-    //->addRouter()
-    // ->addRouter('Zls_Router_PathInfo')
-    // 默认控制器
-    //->setDefaultController('Zls')
-    // 控制器方法前缀
-    ->setMethodPrefix('z')
-    // 设置别名
-    //->setAlias([ 'Http' => \Zls\Action\Http::class ])
     // 设置缓存
     ->setCacheConfig('cache')
     // 设置数据库连接信息
     ->setDatabaseConfig('database')
     // 设置自定义的错误显示控制处理类
     //->setExceptionHandle(new \Exception\Wx())
-    // 错误日志记录，注释掉这行会关闭日志记录，去掉注释则开启日志文件记录,第一个参数是日志文件路径，第二个参数为是否记录404类型异常
-    ->addLoggerWriter(new \Zls\Logger\FileWriter())
-    // 设置session信息
-    ->setSessionConfig('session')
-    // 设置session托管类型 可以直接传入Zls_Session类对象,也可以传入配置文件名称，配置文件里面要返回一个Zls_Session类对象。
-    //->setSessionHandle('sessionHandle')
 ;
 Zls::run();
